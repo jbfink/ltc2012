f118136415a6a150089e074a2ad7e4825d8433cf
jbfink <john.fink@gmail.com>
2011-11-17 10:02:24 -0500

    Add mindmap stub.

---
+++ LTC2012.mm
@@ -0,0 +1,8 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1321542019101" ID="ID_882378609" MODIFIED="1321542022025" TEXT="LTC2012">
+<node CREATED="1321542022617" ID="ID_49065718" MODIFIED="1321542035711" POSITION="right" TEXT="Who am I?"/>
+<node CREATED="1321542028019" ID="ID_302908572" MODIFIED="1321542030027" POSITION="left" TEXT="Who are you?"/>
+<node CREATED="1321542039748" ID="ID_666779717" MODIFIED="1321542055305" POSITION="right" TEXT="Why are two seemingly incongruous topics combined in one talk?"/>
+</node>
+</map>
